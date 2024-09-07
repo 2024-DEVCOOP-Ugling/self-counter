98f20a2c56be723d07580d0537e895600bb8f830
hanjiwon1108 <jwjwhan1108@gmail.com>
2024-09-07 22:42:27 +0900

    product-info-table 개발

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(1);
   return (
     <div className="container">
       <div className="main-box">
@@ -9,13 +10,37 @@ function App() {
           <div className="nav-name">박현준 | 잔액 : 20000원</div>
           <button className="nav-select">상품선택</button>
         </div>
-        <div className="product-info">
-          <span className="product-name">상품이름</span>
-          <div className="product-num-price">
-            <span className="product-num">수량</span>
-            <span className="product-price">상품 가격</span>
-          </div>
-        </div>
+        <table className="product-info-table">
+          <thead>
+            <tr className="product-sort">
+              <th className="product-name-header">상품이름</th>
+              <div className="product-num-price">
+                <th className="product-num-header">수량</th>
+                <th className="product-price-header">상품 가격</th>
+              </div>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="product-name">슈퍼콘 민트초코</td>
+              <td className="product-num">1</td>
+              <td className="product-price">1400</td>
+              <td className="product-plus-minus">
+                <button className="product-plus">+</button>
+                <button className="product-minus">-</button>
+              </td>
+            </tr>
+            <tr>
+              <td className="product-name">꼬미볼 복숭아맛</td>
+              <td className="product-num">2</td>
+              <td className="product-price">1200</td>
+              <td className="product-plus-minus">
+                <button className="product-plus">+</button>
+                <button className="product-minus">-</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
